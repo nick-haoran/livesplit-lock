059983a617aa245a74a5f82c7d6aa3e96db75a11
qihaoran <33177696+nick-haoran@users.noreply.github.com>
2020-11-15 13:47:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main(){
 	string  str = "LiveSplit";
 	HWND pWnd = FindWindow(NULL, str.c_str());
-	SetWindowLong(pWnd, GWL_EXSTYLE, WS_EX_TRANSPARENT | WS_EX_LAYERED);
+	int flag=MessageBox(NULL,"Open Mouse-Penetrate YES/NO", "LiveSplit-Penetrate", MB_YESNO);
+	if(flag==6)SetWindowLong(pWnd, GWL_EXSTYLE, WS_EX_TRANSPARENT | WS_EX_LAYERED);
+	else SetWindowLong(pWnd, GWL_EXSTYLE, NULL);
 }
-
 
 
